7af0f0a1012926e87b27be22185d16c47670d771
Seth Collins <blismatic@gmail.com>
2023-07-07 12:32:27 -0700

    add '--replace' argument to command line handler

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ To trim down a knackly file and remove all `id$` keys, run
 ```bash
 python strip_id.py <filename>
 ```
+
+By default, this creates a new file "`_<filename>`". To replace the file instead, include the `-r` tag
+
+```bash
+python strip_id.py <filename> [-r]
+```
--- strip_id.py
+++ strip_id.py
@@ -1,5 +1,15 @@
+import argparse
 import sys
 import json
+
+def init_argparse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s [filename]",
+        description="Removes any keys with the name \'id$\' from a provided json file."
+    )
+    parser.add_argument("filename", type=str)
+    parser.add_argument("-r", "--replace", action='store_true', help='replace the provided file instead of creating a new file')
+    return parser
 
 def remove_key(d: dict, k: str) -> dict:
     '''Recursively remove all keys with the name 'k' from the nested dictionary 'd'
@@ -16,21 +26,41 @@ def remove_key(d: dict, k: str) -> dict:
             new_dict[key] = value
     return new_dict
 
-# Load the specified file into memory, as a python dictionary
-if len(sys.argv) != 2:
-    print('Sorry, usage must be in the format: \'python strip_id.py <filename>\'')
+# Parse command line arguments
+parser = init_argparse()
+args = parser.parse_args()
+
+filename = args.filename
+period_index = filename.rfind('.')
+if period_index == -1:
+    print(f'Please include the file extension \'.json\'')
+    sys.exit()
+
+extension = filename[period_index:]
+if extension != '.json':
+    print(f'You must provide a .json file, not a \'{extension}\' file')
     sys.exit()
     
-filename = sys.argv[1]
-with open(filename, 'r') as in_file:
-    data = json.load(in_file)
+# Load the json object into memory
+try:
+    with open(filename, 'r') as in_file:
+        data = json.load(in_file)
+except FileNotFoundError:
+    print(f'The file \'{filename}\' does not exist. Are you sure you are typing it correctly?')
+    sys.exit()
+except Exception as e:
+    print(f'Could not parse {filename} into valid JSON. Make sure your file is formatted properly.')
+    sys.exit()
 
 # Remove all 'id$' keys since we cannot predict those
 data = remove_key(data, 'id$')
 
-# Save the altered json as a new file
-with open(f'_{filename}', 'w') as out_file:
+# Save the altered json to a file
+if not args.replace:
+    filename = '_' + filename
+    
+with open(filename, 'w') as out_file:
     stream = json.dumps(data, indent=2)
     out_file.write(stream)
     
-print(f'Success! New file created: _{filename}')
+print(f'Success! New file created: {filename}')
