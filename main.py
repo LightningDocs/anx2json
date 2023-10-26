b0dc0e0ff7d322388156721e81c3a9dc5f51b8ef
Seth Collins <blismatic@gmail.com>
2023-10-26 11:18:03 -0700

    add CLI support for excluding .anx elements from verbose output

--- main.py
+++ main.py
@@ -1,30 +1,72 @@
 import argparse
 from anx_parser import ANX_Parser
+import os
 
 
-def init_argparse() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-i",
-        "--input",
-        required=True,
-        type=argparse.FileType("r", encoding="UTF-8"),
-        help="input file path",
-    )
-    parser.add_argument(
-        "-o",
-        "--output",
-        required=True,
-        type=argparse.FileType("w"),
-        help="output file path",
-    )
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        action="store_true",
-        help="print information about the conversion",
-    )
-    return parser
+def parse_arguments() -> argparse.Namespace:
+    """Return the args Namespace after validating that args have been provided correctly"""
+
+    def init_argparse() -> argparse.ArgumentParser:
+        """Create the ArgumentParser object"""
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "-i",
+            "--input",
+            required=True,
+            type=argparse.FileType("r", encoding="UTF-8"),
+            help="input file path",
+        )
+        parser.add_argument(
+            "-o",
+            "--output",
+            required=True,
+            type=argparse.FileType("w"),
+            help="output file path",
+        )
+        parser.add_argument(
+            "-v",
+            "--verbose",
+            action="store_true",
+            help="print information about the conversion",
+        )
+        parser.add_argument(
+            "-e",
+            "--exclude",
+            nargs="+",
+            help="""specify which .anx answers should be excluded from verbose message (requires verbose). 
+            This should be either:
+                - a path to a .txt file, where each line in the file is the name of an Answer element to be excluded, 
+                - multiple strings, where each string is the name of an Answer element""",
+        )
+        return parser
+
+    parser = init_argparse()
+    args = parser.parse_args()
+
+    # Validate that if exclude was provided, verbose must have also been provided
+    if args.exclude is not None and args.verbose is False:
+        parser.error(
+            "argument -e/--exclude: cannot appear unless argument -v/--verbose is also provided"
+        )
+
+    # Validate that if exclude was a file path, the file exists and is a .txt file
+    if len(args.exclude) == 1:
+        provided_file_path = args.exclude[0]
+        valid_file = (
+            os.path.exists(provided_file_path)
+            and os.path.isfile(provided_file_path)
+            and provided_file_path.endswith(".txt")
+        )
+        if not valid_file:
+            parser.error(
+                f"argument -e/--exclude: can't open '{provided_file_path}': Either didn't end in .txt, or could not find file"
+            )
+
+        # If it was a valid file, convert args.exclude to a list containing each line of the file as a string
+        with open(provided_file_path, "r") as excludefile:
+            args.exclude = [line.strip() for line in excludefile]
+
+    return args
 
 
 def main(args: argparse.Namespace):
@@ -58,5 +100,5 @@ def test(args: argparse.Namespace):
 
 
 if __name__ == "__main__":
-    args = init_argparse().parse_args()
+    args = parse_arguments()
     main(args)
